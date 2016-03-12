691d4256f6ce421bb0b28831a6335926017db8f6
1962449521 <1962449521@qq.com>
2016-03-12 21:02:39 +0800

    银河系行星

---
+++ Solar/Solar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Solar
+//
+//  Created by 胡 帅 on 15/12/1.
+//  Copyright © 2015年 Disney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
