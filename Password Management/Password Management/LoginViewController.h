57c7d9417b706ae1caa5956df82be0f959daa516
1962449521 <1962449521@qq.com>
2016-03-12 22:51:20 +0800

    个人密码管理 plist

---
+++ Password Management/Password Management/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Password Management
+//
+//  Created by Mitty on 15/11/20.
+//  Copyright © 2015年 Disney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ Password Management/Password Management/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Password Management
+//
+//  Created by Mitty on 15/11/20.
+//  Copyright © 2015年 Disney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UIViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
